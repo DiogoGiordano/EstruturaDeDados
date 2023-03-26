ee4b26cadf7d74fccdbac7d6745a8486d45f6002
DiogoGio <diogokmgiordano@gmail.com>
2023-03-26 04:52:16 -0300

    Criação dos Atributos, construtor e Getters

--- idea/tad/Tad.java
+++ idea/tad/Tad.java
@@ -1,4 +1,36 @@
 package idea.tad;
 
 public class Tad {
+
+    public double xa;
+    public double xb;
+    public double ya;
+    public double yb;
+
+    public Tad(double xa, double xb, double ya, double yb){
+
+        this.xa = xa;
+        this.xb = xb;
+        this.ya = ya;
+        this.yb = yb;
+
+    }
+
+    public double getXa() {
+        return xa;
+    }
+
+    public double getXb() {
+        return xb;
+    }
+
+    public double getYa() {
+        return ya;
+    }
+
+    public double getYb() {
+        return yb;
+    }
+
+
 }
