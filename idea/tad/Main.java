dd9f06709859ab1360659c08a45d27b6056bbfdb
DiogoGio <diogokmgiordano@gmail.com>
2023-03-26 22:20:43 -0300

    Criação dos setters e metodo de calculo da distancia

--- idea/tad/Main.java
+++ idea/tad/Main.java
@@ -1,6 +1,8 @@
 package idea.tad;
 
 public class Main {
+    public static void main (String[] args){
 
+    }
 
 }
--- idea/tad/Tad.java
+++ idea/tad/Tad.java
@@ -6,6 +6,7 @@ public class Tad {
     public double xb;
     public double ya;
     public double yb;
+    public double Dab;
 
     public Tad(double xa, double xb, double ya, double yb){
 
@@ -31,6 +32,35 @@ public class Tad {
     public double getYb() {
         return yb;
     }
+
+    public double getDab(){
+        return Dab;
+    }
+
+    public void setXa(double xa){
+        this.xa = xa;
+    }
+
+    public void setXb(double xb){
+        this.xb = xb;
+    }
+
+    public void setYa(double ya){
+        this.ya = ya;
+    }
+
+    public void setYb(double yb){
+        this.yb = yb;
+    }
+
+    public void setDab(double Dab){
+        this.Dab = Dab;
+    }
+
+    public void CalculoDaDistancia (){
+        setDab(Math.sqrt(Math.pow(xb - xa, 2) + Math.pow(yb - ya, 2)));
+    }
+
 
 
 }
