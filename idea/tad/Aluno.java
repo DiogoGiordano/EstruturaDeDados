5f5d0a90d22cb0c0a7159c2d6d6da4a79f701ea2
DiogoGio <diogokmgiordano@gmail.com>
2023-04-03 05:15:34 -0300

    Criação da classe Aluno e seus atributos, gettters e setters, e classe Ialuno e seus metodos abstratos

---
+++ idea/tad/Aluno.java
@@ -0,0 +1,28 @@
+package idea.tad;
+
+public class Aluno {
+
+    private String nome;
+    private double nota;
+    private int idade;
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    private double getNota() {
+        return nota;
+    }
+    private void setNota(double nota) {
+        this.nota = nota;
+    }
+    public int getIdade() {
+        return idade;
+    }
+    private void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+}
---
+++ idea/tad/Ialuno.java
@@ -0,0 +1,10 @@
+package idea.tad;
+
+public interface Ialuno {
+
+    public void adicionar(Aluno al);
+    public int tamanho();
+    public boolean contem(Aluno al);
+    public boolean cheio();
+
+}
